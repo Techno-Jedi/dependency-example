874ed17c0ec7e85cc3711e0ae04d022eddc9d8a7
Alex <aleksandr.leontev.n@mail.ru>
2022-10-11 18:23:24 +0300

    template

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["jest"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "import/prefer-default-export": "off",
+    "max-len": ["error", { ignoreComments: true }],
+  },
+};
